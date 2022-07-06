45bea738e468d62495c5222b2748dec353d0b47f
Noah <nbarne2@wgu.edu>
2022-07-05 18:19:36 -0700

    Added in / route and built requirements using pip freeze > requirements.txt for Docker build

---
+++ datetime_json.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from flask import Flask, jsonify
+
+# Create an instance of the Flask class that is the WSGI application.
+# The first argument is the name of the application module or package,
+# typically __name__ when using a single module.
+app = Flask(__name__)
+
+# Flask route decorators map / and /hello to the hello function.
+# To add other resources, create functions that generate the page contents
+# and add decorators to define the appropriate resource locators for them.
+
+
+@app.route('/')
+def hello():
+    # Render the page
+    # Print Hello World!
+    return "Hello World!"
+
+
+@app.route('/date')
+def date():
+    # Render the page
+    # datetime object containing current date and time
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    dt_string1 = now.strftime("%d/%m/%Y")
+    dt_string2 = now.strftime("%H:%M:%S:%f")
+
+    return jsonify(
+        date=dt_string1,
+        time=dt_string2
+    )
+
+
+if __name__ == '__main__':
+    # Run the app server on localhost:4449
+    app.run('localhost', 4449)
